a55e60a48e0915cf5fca0143ad60fc2f6c11d433
rodrigo.santos <rodrigo.santos@b2wdigital.com>
2016-11-25 11:25:16 -0200

    docker

--- impl/pricing/src/main/java/car/CassandraConfig.java
+++ impl/pricing/src/main/java/car/CassandraConfig.java
@@ -11,78 +11,34 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
-import org.springframework.data.cassandra.config.java.AbstractCassandraConfiguration;
-import org.springframework.data.cassandra.mapping.BasicCassandraMappingContext;
-import org.springframework.data.cassandra.mapping.CassandraMappingContext;
-import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-@Configuration
-@EnableCassandraRepositories
-@EnableSpringDataWebSupport
-public class CassandraConfig extends AbstractCassandraConfiguration {
+@Component
+@ConditionalOnProperty("cassandra.populate")
+public class CassandraConfig{
 
-    @Value("${cassandra.keyspace:price}")
-    private String keyspace;
-    @Value("${cassandra.contactpoints:localhost}")
-    private String contactpoints;
-    @Value("${cassandra.port:9042}")
-    private Integer port;
+    @Value("${cassandra.populate:false}")
+    private Boolean populate;
 
-    @Override
-    protected String getKeyspaceName() {
-        return keyspace;
+    @Data
+    private static class PopulateClass{
+        private List<Car> car;
+        private List<Coupom> coupom;
+        private List<Offer> offer;
     }
 
-    @Override
-    @Bean
-    public CassandraClusterFactoryBean cluster() {
-        final CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
-        cluster.setContactPoints(contactpoints);
-        cluster.setPort(port);
-        return cluster;
-    }
-
-    @Override
-    @Bean
-    public CassandraMappingContext cassandraMapping() throws ClassNotFoundException {
-        return new BasicCassandraMappingContext();
-    }
-
-
-
-    @Component
-    @ConditionalOnProperty("cassandra.populate")
-    public static class Populate{
-
-        @Value("${cassandra.populate:false}")
-        private Boolean populate;
-
-        @Data
-        private static class PopulateClass{
-            private List<Car> car;
-            private List<Coupom> coupom;
-            private List<Offer> offer;
+    @Autowired
+    public void populate(CarRepository carRepository, CoupomRepository coupomRepository, OfferRepository offerRepository, ObjectMapper om) throws IOException {
+        if(populate){
+            PopulateClass data = om.readValue(this.getClass().getResourceAsStream(File.separator + "data.json"), PopulateClass.class);
+            carRepository.save(data.car);
+            offerRepository.save(data.offer);
+            coupomRepository.save(data.coupom);
         }
-
-        @Autowired
-        public void populate(CarRepository carRepository, CoupomRepository coupomRepository, OfferRepository offerRepository, ObjectMapper om) throws IOException {
-            if(populate){
-                PopulateClass data = om.readValue(this.getClass().getResourceAsStream(File.separator + "data.json"), PopulateClass.class);
-                carRepository.save(data.car);
-                offerRepository.save(data.offer);
-                coupomRepository.save(data.coupom);
-            }
-        }
-
     }
 
 }
